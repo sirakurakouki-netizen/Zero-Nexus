101370b3eb74828a32ba73698335a0ef26ae0392
sirakurakouki-netizen <sirakurakouki@gmail.com>
2026-02-01 07:51:28 +0000

    G

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
 const path = require('path');
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
 
-app.use(express.static('public'));
+// ルートにあるファイルを静的ファイルとして公開
+app.use(express.static('.'));
 
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
+// ルートアクセスでindex.htmlを返す
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+// ポート番号はReplitとFirebase両方に対応しやすい8080か3000を使用
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
